fbaae952b833b80df0748f93291eaa33db0e0b28
Keronei <lincolnkeronei95@gmail.com>
2023-08-02 16:10:07 +0300

    Implement various clipping methods

---
+++ app/src/main/java/com/keronei/viewclipping/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.keronei.viewclipping
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(ClippedView(this))
+    }
+}
